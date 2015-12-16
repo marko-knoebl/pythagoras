522acdf5e1a725f38aa178b357d0557e60432185
Marko Knöbl <marko.kn@gmail.com>
2015-12-16 21:39:25 +0100

    initial commit

---
+++ controllers.js
@@ -0,0 +1,53 @@
+'use strict';
+
+var pythagorasApp = angular.module('pythagorasApp', ['ngMaterial', 'ngMessages']);
+
+pythagorasApp.controller('PythagorasCtrl', function($scope, $http) {
+  // default date range: 1 month
+
+  $scope.correct = 0;
+  $scope.wrong = 0;
+
+  var randint = function(min, max) {
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+  };
+
+  var newQuestion = function() {
+    var lengths = pythagoreanTriple();
+    $scope.a = lengths.a;
+    $scope.b = lengths.b;
+    $scope.c = lengths.c;
+    $scope.unknown = ['a','b', 'c', 'c'][randint(0, 3)];
+    $scope[$scope.unknown] = '?';
+  };
+  
+  var pythagoreanTriple = function() {
+    var m = randint(2, 7);
+    var n = randint(1, m-1)
+    var a = m*m - n*n;
+    var b = 2*m*n;
+    var c = m*m + n*n;
+    var lengths = {a:a, b:b, c:c};
+    return lengths;
+  }
+
+  newQuestion();
+
+  $scope.checkAnswer = function() {
+    var answer = parseInt($scope.answer);
+    var l = {
+      a: $scope.a,
+      b: $scope.b,
+      c: $scope.c
+    };
+    l[$scope.unknown] = answer;
+    if (l.a*l.a + l.b*l.b === l.c*l.c) {
+      $scope.correct ++;
+      $scope.answer = '';
+      newQuestion();
+    } else {
+      $scope.wrong ++;
+      $scope.answer = '';
+    }
+  }
+});
